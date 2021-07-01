e1948e9d51ac38b84ce580316398c0ad07e84766
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2021-07-01 21:19:45 +0200

    Fixed Context.

--- src/main/java/pl/matkoc/DBUtil/DBUtil.java
+++ src/main/java/pl/matkoc/DBUtil/DBUtil.java
@@ -10,15 +10,17 @@ import java.sql.SQLException;
 
 public class DBUtil {
     private static DataSource dataSource;
-    public static Connection getConn() throws SQLException {
-        return getInstance().getConnection();
-    }
+
     private static DataSource getInstance() {
         if(dataSource == null) {
             try {
                 Context ctx = new InitialContext();
-                dataSource = (DataSource)ctx.lookup("java:comp/env/jdbc/crm_car_repair");
+                Context envContext = (Context)ctx.lookup("java:/comp/env");
+                dataSource = (DataSource)envContext.lookup("jdbc/cmr_car_repair");
             } catch (NamingException e) {
                 e.printStackTrace();}}
         return dataSource;}
+    public static Connection getConn() throws SQLException {
+        return getInstance().getConnection();
+    }
 }
