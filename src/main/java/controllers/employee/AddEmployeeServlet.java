41a07da387a3e13d61d26b9c8bf82e478c704a35
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2021-07-08 20:59:12 +0200

    I wrote code to collect data from the form, created new employee object and saved it to the database.

--- src/main/java/controllers/employee/AddEmployeeServlet.java
+++ src/main/java/controllers/employee/AddEmployeeServlet.java
@@ -1,5 +1,8 @@
 package controllers.employee;
 
+import dao.EmployeeDao;
+import model.Employee;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -16,6 +19,26 @@ public class AddEmployeeServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request,
                           HttpServletResponse response) throws ServletException, IOException {
+
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
+        int number = Integer.parseInt(request.getParameter("phone_number"));
+        String email = request.getParameter("email");
+        String notes = request.getParameter("notes");
+        double rate = Double.parseDouble(request.getParameter("hourly_rate"));
+
+        Employee employee = new Employee();
+        employee.setFirst_name(name);
+        employee.setLast_name(surname);
+        employee.setNumber_phone(number);
+        employee.setEmail(email);
+        employee.setNotes(notes);
+        employee.setHourly_rate(rate);
+
+        EmployeeDao employeeDao = new EmployeeDao();
+        employeeDao.create(employee);
+
+        response.sendRedirect("/owner/ownerPage.jsp");
 
     }
 }
