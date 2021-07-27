61ab1f4539170510201e4a21d98c8c626c3a32cd
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2021-07-27 20:39:06 +0200

    Repaired statement.

--- src/main/java/dao/CarDao.java
+++ src/main/java/dao/CarDao.java
@@ -11,8 +11,8 @@ import java.sql.SQLException;
 public class CarDao {
 
     private final String CREATE_CAR_QUERY =
-            "INSERT INTO car(id_car,id_customer,model,mark,year_of_production,registration_number," +
-                    "technical_inspection_year, technical_inspection_month, technical_inspection_day) VALUES(?,?,?,?,?,?,?,?,?)";
+            "INSERT INTO car(id_customer,model,mark,year_of_production,registration_number," +
+                    "technical_inspection_year, technical_inspection_month, technical_inspection_day) VALUES(?,?,?,?,?,?,?,?)";
 
 
     public Car create(Car car){
@@ -21,14 +21,13 @@ public class CarDao {
             PreparedStatement statement = connection.prepareStatement(CREATE_CAR_QUERY,
                     PreparedStatement.RETURN_GENERATED_KEYS);
             statement.setInt(1, car.getId_customer());
-            statement.setInt(2, car.getId_car());
-            statement.setString(3,car.getMark());
-            statement.setString(4,car.getModel());
-            statement.setInt(5,car.getYear_of_production());
-            statement.setString(6,car.getRegistration_number());
-            statement.setInt(7,car.getTechnical_inspection_year());
-            statement.setInt(8,car.getTechnical_inspection_month());
-            statement.setInt(9,car.getTechnical_inspection_day());
+            statement.setString(2,car.getMark());
+            statement.setString(3,car.getModel());
+            statement.setInt(4,car.getYear_of_production());
+            statement.setString(5,car.getRegistration_number());
+            statement.setInt(6,car.getTechnical_inspection_year());
+            statement.setInt(7,car.getTechnical_inspection_month());
+            statement.setInt(8,car.getTechnical_inspection_day());
             statement.executeUpdate();
             ResultSet resultSet = statement.getGeneratedKeys();
             while(resultSet.next()){
